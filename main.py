e7c80512a3b3c5c5ed6c1522425cfe846210196b
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2026-03-17 15:16:06 -0400

    changing data types for duration, profile, and start time

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import numpy as np
+import matplotlib 
+import matplotlib.pyplot as mpl
 
 viewingActivity = pd.read_csv("ViewingActivity.csv", header=0, sep=",")
 
 
-print(viewingActivity.shape)
+viewingActivity['Duration']= viewingActivity['Duration'].astype('timedelta64[us]')
+viewingActivity['Start Time']= viewingActivity['Start Time'].astype('datetime64[ns]')
+viewingActivity['Profile Name']= viewingActivity['Profile Name'].astype('category')
 
-print(viewingActivity["Profile Name"].unique())
 
-print(viewingActivity["Device Type"].unique())
+print(viewingActivity.info())
 
-print(viewingActivity.dtypes)
+# print(viewingActivity["Profile Name"].value_counts())
+
+# viewingActivity["Profile Name"].value_counts().plot(kind= 'bar')
+# mpl.show()
