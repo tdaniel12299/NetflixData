79a76d3dd3f5492c09a802510a64a536ec282b00
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2026-03-17 15:39:41 -0400

    data cleaning: supplemental video type NAN changed to not supplemental

--- main.py
+++ main.py
@@ -9,11 +9,20 @@ viewingActivity = pd.read_csv("ViewingActivity.csv", header=0, sep=",")
 viewingActivity['Duration']= viewingActivity['Duration'].astype('timedelta64[us]')
 viewingActivity['Start Time']= viewingActivity['Start Time'].astype('datetime64[ns]')
 viewingActivity['Profile Name']= viewingActivity['Profile Name'].astype('category')
-
+viewingActivity["Supplemental Video Type"] = viewingActivity["Supplemental Video Type"].fillna('NOT SUPPLEMENTAL')
 
 print(viewingActivity.info())
 
-# print(viewingActivity["Profile Name"].value_counts())
+
+print(viewingActivity["Supplemental Video Type"].unique())
+
+
+#print(viewingActivity["Supplemental Video Type"].sample(n=10))
+#print(viewingActivity["Attributes"].sample(n=10))
+
+#print(viewingActivity["Supplemental Video Type"].value_counts)
+
+#print(viewingActivity["Profile Name"].value_counts())
 
 # viewingActivity["Profile Name"].value_counts().plot(kind= 'bar')
 # mpl.show()
